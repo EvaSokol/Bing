78fd667f9adeca49e78ca21ee88497a475befabe
EvaSokol <esokolyanskaya@lohika.com>
2014-08-13 22:47:43 +0300

    Compare works correctly
    Support class is added. Data saving works. Compare should have been
    developed for logging.

--- src/second.java
+++ src/second.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,25 +26,43 @@ import org.w3c.dom.Element;
 public class second {
 
 	static WebDriver driver;
-	static String locale;
-	static List<WebElement> links;
+	static String locale = "en";
+	static String baseUrl = "http://www.bing.com/";
+	static List<trans> EnglishElements = new ArrayList<trans>();
+	static List<trans> LocalizedElements = new ArrayList<trans>();
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		startBrowser();
-		getDataToList();
-		writeToXml();
+		
+		startBrowser(locale);
+		getDataToList(EnglishElements);
 		driver.close();
+		locale = changeLocale();
+		startBrowser(locale);
+		getDataToList(LocalizedElements);
+		driver.close();
+		
+		compairing();
 		
 	}
 	
-	public static void startBrowser() {
+	private static void compairing() {
+						
+		AbstractMap<String, String> locList = new TreeMap<String, String>();
+		for (trans t : LocalizedElements) {
+			locList.put(t.getId(), t.getData());
+		}
 		
-		String baseUrl = "http://www.bing.com/";
+		for (trans element: EnglishElements) {
+			
+			System.out.println("English: " + element.getData());
+			System.out.println("Localized: " + locList.get(element.getId()));
+		}
+		
+	}
+
+	public static void startBrowser(String locale) {
+				
 		FirefoxProfile profile = new FirefoxProfile();
-		
-		locale = changeLocale();
 		profile.setPreference("intl.accept_languages", locale);
 		driver = new FirefoxDriver(profile);
 		driver.get(baseUrl);
@@ -65,58 +86,83 @@ public class second {
 		}
 	}
 	
-	static void getDataToList() {
-		
-		links = driver.findElements(By.xpath(".//*/a"));
-		for (WebElement link : links) {
-			if (link.isDisplayed() == true & !(link.getText().isEmpty()) )
+	static void getDataToList(List<trans> Elements) {
+		System.out.println("Locale is " + locale);
+		List<WebElement> baseList = driver.findElements(By.xpath(".//*/a"));
+				
+		for (WebElement link : baseList) {
+			if (link.isDisplayed() == true & !(link.getText().isEmpty()) ) {
 				System.out.println(link.getAttribute("h") + " : " + link.getText());
+				Elements.add(new trans(link.getAttribute("h"), link.getText()));
+				}
 		}
 	}
 	
 	static void writeToXml() {
-		try {
-			
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-	 		Document doc = docBuilder.newDocument();
-			
-			Element rootElement = doc.createElement(locale);
-			doc.appendChild(rootElement);
-						
-			Element page = doc.createElement("HomePage");
-			rootElement.appendChild(page);
-			
-			System.out.println("Write to file. Current locale is: " + locale);
-			String attribute;						
-			for (WebElement link : links) {
-				if (link.isDisplayed() == true & !(link.getText().isEmpty()) ) {
-					
-					attribute = link.getAttribute("h");
-					attribute = attribute.replace(',', '.');
-					attribute = attribute.replace('=', '.');
-							
-					System.out.println(attribute + " * " + link.getText());
-					page.setAttribute(attribute, link.getText());
-								
-				}
-							
-			}
-		
-				TransformerFactory transformerFactory = TransformerFactory.newInstance();
-				Transformer transformer = transformerFactory.newTransformer();
-				DOMSource source = new DOMSource(doc);
-				StreamResult result = new StreamResult(new File("languages1.xml"));
-		 						 
-				transformer.transform(source, result);
-		 
-				System.out.println("File saved!");
-				System.out.println("----------------------------");
-		
-		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		  } catch (TransformerException tfe) {
-			tfe.printStackTrace();
-		  }
+//		try {
+//			
+//			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+//			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+//	 		Document doc = docBuilder.newDocument();
+//			
+//			Element rootElement = doc.createElement(locale);
+//			doc.appendChild(rootElement);
+//						
+//			Element page = doc.createElement("HomePage");
+//			rootElement.appendChild(page);
+//			
+//			System.out.println("Write to file. Current locale is: " + locale);
+//			String attribute;						
+//			for (WebElement link : links) {
+//				if (link.isDisplayed() == true & !(link.getText().isEmpty()) ) {
+//					
+//					attribute = link.getAttribute("h");
+//					attribute = attribute.replace(',', '.');
+//					attribute = attribute.replace('=', '.');
+//							
+//					System.out.println(attribute + " * " + link.getText());
+//					page.setAttribute(attribute, link.getText());
+//								
+//				}
+//							
+//			}
+//		
+//				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+//				Transformer transformer = transformerFactory.newTransformer();
+//				DOMSource source = new DOMSource(doc);
+//				StreamResult result = new StreamResult(new File("languages1.xml"));
+//		 						 
+//				transformer.transform(source, result);
+//		 
+//				System.out.println("File saved!");
+//				System.out.println("----------------------------");
+//		
+//		} catch (ParserConfigurationException pce) {
+//			pce.printStackTrace();
+//		  } catch (TransformerException tfe) {
+//			tfe.printStackTrace();
+//		  }
+	}
+
+	
+	
+	
+}
+
+class trans {
+	String id;
+	String data;
+	
+	trans (String id, String data) {
+		this.id = id;
+		this.data = data;
+	}
+	
+	String getId(){
+		return id;
+	}
+	
+	String getData(){
+		return data;
 	}
 }
